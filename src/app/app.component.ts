3b2c1dc41f2b416a3bcf562f504ea46c52971586
Xiaoxia <jinxiaoxiaclaire@gmail.com>
2024-12-04 14:59:51 +0000

    Create a new angular todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule} from '@angular/forms';
+import { NgClass, NgFor } from '@angular/common';
 
+export interface TodoItem{
+  id:number;
+  task:string;
+  completed:boolean;
+}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-todo';
+  todolist: TodoItem[] = [];
+  newTask: string ='';
+
+  addTask():void{
+    if(this.newTask.trim() !== ''){
+      const newTodoItem:TodoItem = {
+        id : Date.now(),
+        task : this.newTask,
+        completed: false
+      }
+
+      this.todolist.push(newTodoItem)
+      //after clicking, the input should be cleared
+      this.newTask = ''
+    }
+  }
+
+  toggleComplete(index: number):void{
+    // console.log(index)
+    this.todolist[index].completed =!this.todolist[index].completed
+    // console.log(this.todolist)
+  }
+
+  deleteTask(id: number):void{
+    this.todolist = this.todolist.filter(item => item.id !== id)
+    // console.log(this.todolist)
+  }
+
+
 }
